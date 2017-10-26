efbbdfc0afa74c82086c7a4929ff9378983fd734
zavitar <you@example.com>
2017-10-25 20:32:55 -0700

    examen

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zavitar/Documentos/proyectos/examen_desarroweb_1/venv/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
