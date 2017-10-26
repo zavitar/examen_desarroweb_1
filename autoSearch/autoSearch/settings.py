e849255bf8c29a110fd53b8d99d7faacf89f8b09
zavitar <you@example.com>
2017-10-25 23:40:13 -0700

    commit

--- autoSearch/autoSearch/settings.py
+++ autoSearch/autoSearch/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'carros'
+     ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,5 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRP=[os.path.join(BASE.DIR, "S_Storage")]
-STATIC_ROOT = os.path.join(BASE_DIR, "Static_Serve")
+
--- autoSearch/carros/views.py
+++ autoSearch/carros/views.py
@@ -4,3 +4,5 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def lago(request):
+	return render(request,"home.html")
